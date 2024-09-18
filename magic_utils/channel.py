ac416ae4e63aecc7b85a56b2930686970265cdcf
MrCode200 <100614430+MrCode200@users.noreply.github.com>
2024-09-18 11:02:08 +0000

    added mmore functions to the channel class

--- magic_utils/channel.py
+++ magic_utils/channel.py
@@ -1,16 +1,28 @@
+from functools import total_ordering
+
+@total_ordering 
 class channel():
     def __init__(self):
         self.subscribers = list[callable]
 
-    def subscribe(self, new_subscriber: callable | tuple[callable]):
-        self.subscribers.append(new_subscriber) 
+    def subscribe(self, new_subscriber: callable, *args, **kwargs):
+        self.subscribers.append((new_subscriber, args, kwargs)) 
 
-    def unsubscribe(self, subscriber: callable | tuple[callable]):
-        self.subscribers.remove(subscriber) 
+    def unsubscribe(self, subscriber: callable):
+        self.subscribers = [(sub, args, kwargs) for sub, args, kwargs in self.subscribers if sub[0] != subscriber] 
 
     def clear(self):
         self.subscribers = []
 
-    def call(self):
-        for sub in self.subscribers:
-            sub() 
+    def notify(self):
+        for sub, args, kwargs in self.subscribers:
+            sub(args, kwargs) 
+
+    def __getitem__(self, func: callable):
+         return [(sub, args, kwargs) for sub, args, kwargs in self.subscribers if func == sub]
+
+    def __len__(self):
+        return len(self.subscribers)
+    
+    def __eq__(self, other):
+        return self.subscribers == other.subscribers
