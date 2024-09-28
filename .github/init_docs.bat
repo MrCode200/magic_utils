6f6f824b1f303c2de98a755a08e64b1d1cf7761f
MrCode200 <navidyaghmaei@gmail.com>
2024-09-29 00:53:17 +0200

    updated bat file

--- .github/init_docs.bat
+++ .github/init_docs.bat
@@ -18,9 +18,9 @@ powershell -Command "(Get-Content 'index.rst')[0..11] + 'modules' + (Get-Content
 echo !GREEN! SUCCESSFUL [3/5]: Modified index.rst !RESET!
 
 :: Modify extensions in config.py
-powershell -Command "(Get-Content 'conf.py') | ForEach-Object { if ($_ -match '^extensions =') { 'extensions = [\"sphinx.ext.todo\", \"sphinx.ext.viewcode\", \"sphinx.ext.autodoc\"]' } else { $_ } } | Set-Content 'config.py'" || (echo !RED!BOLD FAILED: Couldn't modify config.py !RESET! & exit /b 1)
+powershell -Command "(Get-Content 'conf.py') | ForEach-Object { if ($_ -match '^extensions =') { 'extensions = [\"sphinx.ext.todo\", \"sphinx.ext.viewcode\", \"sphinx.ext.autodoc\"]' } else { $_ } } | Set-Content 'conf.py'" || (echo !RED!BOLD FAILED: Couldn't modify config.py !RESET! & exit /b 1)
 echo !GREEN! SUCCESSFUL [4/5]: Modified extensions in config.py !RESET!
 
 :: Modify theme in config.py
-powershell -Command "(Get-Content 'config.py') | ForEach-Object { if ($_ -match '^html_theme =') { 'html_theme = \"sphinx_rtd_theme\"' } else { $_ } } | Set-Content 'config.py'" || (echo !RED!BOLD FAILED: Couldn't modify theme in config.py !RESET! & exit /b 1)
+powershell -Command "(Get-Content 'conf.py') | ForEach-Object { if ($_ -match '^html_theme =') { 'html_theme = \"sphinx_rtd_theme\"' } else { $_ } } | Set-Content 'conf.py'" || (echo !RED!BOLD FAILED: Couldn't modify theme in config.py !RESET! & exit /b 1)
 echo !GREEN! SUCCESSFUL [5/5]: Modified theme in config.py !RESET!
