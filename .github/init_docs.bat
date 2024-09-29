f406ba038cba3661e326100d7cf43dee2d869abf
MrCode200 <navidyaghmaei@gmail.com>
2024-09-29 08:49:40 +0200

    updated bat file

--- .github/init_docs.bat
+++ .github/init_docs.bat
@@ -1,26 +1,73 @@
 @ECHO OFF
-:: sphinx-quickstart command
-sphinx-quickstart || (echo !RED!BOLD FAILED: sphinx-quickstart did not run !RESET! & exit /b 1)
-echo !GREEN! SUCCESSFUL [1/5]: sphinx-quickstart completed !RESET!
+setlocal
 
-:: Move up one directory
+:: Step 1: Run sphinx-quickstart
+sphinx-quickstart || (
+    echo !RED!BOLD FAILED: sphinx-quickstart did not run !RESET!
+    exit /b 1
+)
+echo !GREEN! SUCCESSFUL [1/6]: sphinx-quickstart completed !RESET!
+
+:: Step 2: Move up one directory
 cd ..
 
-:: sphinx-apidoc command
-sphinx-apidoc -o docs . || (echo !RED!BOLD FAILED: Couldn't generate rst files !RESET! & exit /b 1)
-echo !GREEN! SUCCESSFUL [2/5]: Created rst files !RESET!
+:: Step 3: Run sphinx-apidoc to generate rst files
+sphinx-apidoc -o docs . || (
+    echo !RED!BOLD FAILED: Couldn't generate rst files !RESET!
+    exit /b 1
+)
+echo !GREEN! SUCCESSFUL [2/6]: Created rst files !RESET!
 
-:: Change to docs directory
-cd docs
+:: Step 4: Change directory to 'docs'
+cd docs || (
+    echo !RED!BOLD FAILED: Couldn't change directory to docs !RESET!
+    exit /b 1
+)
 
-:: Insert text into index.rst
-powershell -Command "(Get-Content 'index.rst')[0..11] + 'modules' + (Get-Content 'index.rst')[12..$null] | Set-Content 'index.rst'" || (echo !RED!BOLD FAILED: Couldn't modify index.rst !RESET! & exit /b 1)
-echo !GREEN! SUCCESSFUL [3/5]: Modified index.rst !RESET!
+:: Step 5: Delete lines 8 to 12 and
+powershell -Command ^
+    "$content = Get-Content 'index.rst';" ^
+    "$newContent = $content[0..7] + $content[13..($content.Count - 1)];" ^
+    "$newContent | Set-Content 'index.rst';"
 
-:: Modify extensions in config.py
-powershell -Command "(Get-Content 'conf.py') | ForEach-Object { if ($_ -match '^extensions =') { 'extensions = [\"sphinx.ext.todo\", \"sphinx.ext.viewcode\", \"sphinx.ext.autodoc\"]' } else { $_ } } | Set-Content 'conf.py'" || (echo !RED!BOLD FAILED: Couldn't modify config.py !RESET! & exit /b 1)
-echo !GREEN! SUCCESSFUL [4/5]: Modified extensions in config.py !RESET!
+:: Check ERRORLEVEL after the PowerShell command
+if ERRORLEVEL 1 (
+    echo !RED! FAILED: Couldn't delete lines in index.rst !RESET!
+    exit /b 1
+)
+echo !GREEN! SUCCESSFUL [3/6]: Deleted lines in index.rst !RESET!
 
-:: Modify theme in config.py
-powershell -Command "(Get-Content 'conf.py') | ForEach-Object { if ($_ -match '^html_theme =') { 'html_theme = \"sphinx_rtd_theme\"' } else { $_ } } | Set-Content 'conf.py'" || (echo !RED!BOLD FAILED: Couldn't modify theme in config.py !RESET! & exit /b 1)
-echo !GREEN! SUCCESSFUL [5/5]: Modified theme in config.py !RESET!
+:: Insert 'modules' into index.rst
+powershell -Command ^
+    "$content = Get-Content 'index.rst';" ^
+    "$newContent = $content[0..11] + '    modules' + $content[12..($content.Count - 1)];" ^
+    "$newContent | Set-Content 'index.rst';"
+
+:: Check ERRORLEVEL after the PowerShell command
+if ERRORLEVEL 1 (
+    echo !RED! FAILED: Couldn't insert 'modules' into index.rst !RESET!
+    exit /b 1
+)
+echo !GREEN! SUCCESSFUL [4/6]: Inserted 'modules' into index.rst !RESET!
+
+:: Step 6: Modify extensions in conf.py
+powershell -Command ^
+    "(Get-Content 'conf.py') | ForEach-Object { if ($_ -match '^extensions =') { 'extensions = [\"sphinx.ext.todo\", \"sphinx.ext.viewcode\", \"sphinx.ext.autodoc\"]' } else { $_ } } | Set-Content 'conf.py';"
+
+if ERRORLEVEL 1 (
+    echo !RED! FAILED: Couldn't modify extensions in config.py !RESET!
+    exit /b 1
+)
+echo !GREEN! SUCCESSFUL [5/6]: Modified extensions in conf.py !RESET!
+
+:: Step 7: Modify theme in conf.py
+powershell -Command ^
+    "(Get-Content 'conf.py') | ForEach-Object { if ($_ -match '^html_theme =') { 'html_theme = \"sphinx_rtd_theme\"' } else { $_ } } | Set-Content 'conf.py';"
+
+if ERRORLEVEL 1 (
+    echo !RED! FAILED: Couldn't modify theme in config.py !RESET!
+    exit /b 1
+)
+echo !GREEN! SUCCESSFUL [6/6]: Modified theme in conf.py !RESET!
+
+endlocal
