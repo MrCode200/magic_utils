d51bf810344f41d24b7e1f1acf71120aff35977d
Navid <navidyaghmaei1@gmail.com>
2025-09-27 18:24:59 +0200

    fix(core): Update init file
    Update init to include all modules.
    
    - Update version to 0.2.0
    - Expose Registry and setup_logger
    - Add metadata (author, license, title)

--- magic_utils/__init__.py
+++ magic_utils/__init__.py
@@ -1,8 +1,12 @@
-__Version__ = "0.0.1"
-__Author__ = "MrCode200"
+"""Magic Utils - A collection of useful utility classes and functions."""
+
+__title__ = "magic_utils"
+__version__ = "0.2.0"
+__author__ = "MrCode200"
+__license__ = "MIT"
 
 from .eventManager import EventManager
 from .registryManager import Registry
 from .magicLogger import setup_logger
 
-__all__ = ["EventManager"]
+__all__ = ["EventManager", "Registry", "setup_logger"]
